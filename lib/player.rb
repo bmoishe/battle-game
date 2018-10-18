8868e7882baa7fce913df27595c054a9eb61ebbf
bmoishe <moishebarnes@gmail.com>
2018-10-18 14:30:58 +0100

    Implementing Hit Points

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,18 @@
 class Player
-  attr_reader :name
+ DEFAULT_HIT_POINTS = 60
+  attr_reader :name, :hp
 
-  def initialize(name)
+  def initialize(name, hp = DEFAULT_HIT_POINTS)
     @name = name
+    @hp = hp
   end
+
+  def attack(player)
+    player.take_damage
+  end
+
+  def take_damage
+    @hp -= 10
+  end
+
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,10 +2,30 @@ require 'player'
 
 describe Player do
   subject(:dave) { Player.new('Dave') }
+  subject(:mittens) { Player.new('Mittens') }
 
   describe '#name' do
     it 'returns the name' do
       expect(dave.name).to eq 'Dave'
     end
   end
-end
+
+  describe '#hit_points' do
+    it 'returns the hit points' do
+      expect(dave.hp).to eq described_class::DEFAULT_HIT_POINTS
+    end
+  end
+
+  describe '#attack' do
+    it 'damages the player' do
+      expect(mittens).to receive(:take_damage)
+      dave.attack(mittens)
+    end
+  end
+
+  describe '#receive_damage' do
+    it 'reduces the player hit points' do
+      expect { dave.take_damage }.to change { dave.hp }.by(-10)
+    end
+  end
+ end
