8559c739ff6ed4863ab0b9bc3372d846be9e051b
bmoishe <moishebarnes@gmail.com>
2018-10-18 11:29:19 +0100

    Extracting Logic to the Model

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,3 +1,6 @@
+# As Player 1,
+# So I can win a game of Battle,
+# I want to attack Player 2, and I want to get a confirmation
 feature 'Attacking' do
   scenario 'attack Player 2' do
     sign_in_and_play
